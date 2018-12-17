debe8ed4b47be3839f2da08db02b53607799acad
Dima Rusyy <dima.ru.com@gmail.com>
2018-12-17 20:25:30 +0200

    - initial commit

---
+++ include/boost/iostreams/filters/cryptof.hpp
@@ -0,0 +1,14 @@
+#ifndef BOOST_IOSTREAMS_CRYPTOF_HPP_INCLUDED
+#define BOOST_IOSTREAMS_CRYPTOF_HPP_INCLUDED
+
+#if defined(_MSC_VER)
+# pragma once
+#endif              
+
+namespace boost {
+	namespace iostreams {
+
+	}
+}
+
+#endif  /*BOOST_IOSTREAMS_CRYPTOF_HPP_INCLUDED*/
